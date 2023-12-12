4636dac210a626af29811011e1c36bb03731d461
rambo1111 <vibhawspark@gmail.com>
2023-12-12 22:36:44 +0530

    300

---
+++ code/train_classifier.py
@@ -0,0 +1,32 @@
+import pickle
+import os
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+import numpy as np
+
+
+data_dict = pickle.load(open('./data.pickle', 'rb'))
+
+data = np.asarray(data_dict['data'])
+labels = np.asarray(data_dict['labels'])
+
+X_train, X_test, y_train, y_test = train_test_split(data, labels, test_size=0.2, shuffle=True, stratify=labels)
+
+model = RandomForestClassifier()
+
+model.fit(X_train, y_train)
+
+y_predict = model.predict(X_test)
+
+score = accuracy_score(y_predict, y_test)
+
+print('{}% of samples were classified correctly !'.format(score * 100))
+
+f = open('model.p', 'wb')
+pickle.dump({'model': model}, f)
+f.close()
+
+second_model_file_path = os.path.join('project','model.p')
+with open(second_model_file_path, 'wb') as f:
+    pickle.dump({'model': model}, f)
